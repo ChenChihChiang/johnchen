6cf685c28ff33339857d45c79ab78f8bff21a36a
Chih Chiang Chen <john.c.chen@delta.com.tw>
2020-04-08 15:48:24 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,9 +8,16 @@ pipeline {
         steps {
           script{
             sh(label: 'Get GIT_HASH', returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
-            def util = new common.utils()
-            echo "${util.getVersion(env.BUILD_NUMBER,env.GIT_COMMIT)}"
+		  
+            //def util = new common.utils()
+            //echo "${util.getVersion(env.BUILD_NUMBER,env.GIT_COMMIT)}"
             echo "${notify.call('Hello')}"
+	    
+            def loopscript = libraryResource 'loop.sh'
+            sh loopscript
+		  
+            def datajson = libraryResource 'data.json'
+	    echo ${datajson}
           }
         }
       }
